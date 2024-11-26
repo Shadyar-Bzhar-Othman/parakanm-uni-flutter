4ec52b78a78d30691088218348e0cbcf19ae74b0
Shadyar Bzhar Othman <shadyarbzhar22@gmail.com>
2024-11-26 20:33:35 +0300

    Initial commit

---
+++ lib/utils/data.dart
@@ -0,0 +1,124 @@
+const Map<String, dynamic> currentUser = {
+  "id": 1,
+  "name": "Shadyar",
+  "email": "shadyar@gmail.com",
+  "password": "12121212",
+};
+
+const List<Map<String, dynamic>> accounts = [
+  {
+    "id": 1,
+    "name": "Shadyar",
+    "email": "shadyar@gmail.com",
+    "password": "12121212",
+  },
+  {
+    "id": 2,
+    "name": "Lava",
+    "email": "lava@gmail.com",
+    "password": "12121212",
+  },
+  {
+    "id": 3,
+    "name": "Hezha",
+    "email": "hezha@gmail.com",
+    "password": "12121212",
+  },
+];
+
+const List<Map<String, dynamic>> categories = [
+  {
+    "id": 1,
+    "name": "Shopping",
+    "icon": "",
+  },
+  {
+    "id": 2,
+    "name": "Food",
+    "icon": "",
+  },
+  {
+    "id": 3,
+    "name": "Transport",
+    "icon": "",
+  },
+  {
+    "id": 4,
+    "name": "Health",
+    "icon": "",
+  },
+  {
+    "id": 5,
+    "name": "Education",
+    "icon": "",
+  },
+  {
+    "id": 6,
+    "name": "Entertainment",
+    "icon": "",
+  },
+  {
+    "id": 7,
+    "name": "Others",
+    "icon": "",
+  }
+];
+
+const List<Map<String, dynamic>> expenses = [
+  {
+    "userId": 1,
+    "categoryId": 1,
+    "id": 1,
+    "title": "Shopping",
+    "amount": 100,
+    "date": "2021-09-01",
+  },
+  {
+    "userId": 1,
+    "categoryId": 2,
+    "id": 2,
+    "title": "Food",
+    "amount": 50,
+    "date": "2021-09-02",
+  },
+  {
+    "userId": 1,
+    "categoryId": 3,
+    "id": 3,
+    "title": "Transport",
+    "amount": 20,
+    "date": "2021-09-03",
+  },
+  {
+    "userId": 1,
+    "categoryId": 4,
+    "id": 4,
+    "title": "Health",
+    "amount": 30,
+    "date": "2021-09-04",
+  },
+  {
+    "userId": 1,
+    "categoryId": 5,
+    "id": 5,
+    "title": "Education",
+    "amount": 40,
+    "date": "2021-09-05",
+  },
+  {
+    "userId": 1,
+    "categoryId": 6,
+    "id": 6,
+    "title": "Entertainment",
+    "amount": 60,
+    "date": "2021-09-06",
+  },
+  {
+    "userId": 1,
+    "categoryId": 7,
+    "id": 7,
+    "title": "Others",
+    "amount": 70,
+    "date": "2021-09-07",
+  },
+];
